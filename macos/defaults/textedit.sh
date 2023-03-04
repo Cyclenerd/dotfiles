a3df62c1d67e8423143d50bcbeea5423574642ca
Nils <git@nkn-it.de>
2023-03-04 08:50:11 +0100

    text edit

--- macos/defaults/textedit.sh
+++ macos/defaults/textedit.sh
@@ -12,9 +12,9 @@ defaults write com.apple.TextEdit "RichText" -bool "false"
 # Text settings
 defaults write com.apple.TextEdit "CheckSpellingAsYouTypeEnabledInRichTextOnly" -bool "true"
 defaults write com.apple.TextEdit "CheckSpellingWhileTyping" -bool "false"
-defaults write com.apple.TextEdit "NSFixedPitchFont" "SourceCodePro-Regular";
+defaults write com.apple.TextEdit "NSFixedPitchFont" -string "SourceCodePro-Regular";
 defaults write com.apple.TextEdit "NSFixedPitchFontSize" -int "16";
-defaults write com.apple.TextEdit "NSFontPanelAttributes" "1, 0";
-defaults write com.apple.TextEdit "NSNavLastRootDirectory" "~/Desktop";
+defaults write com.apple.TextEdit "NSFontPanelAttributes" -string "1, 0";
+defaults write com.apple.TextEdit "NSNavLastRootDirectory" -string "~/Desktop";
 
 killall TextEdit
