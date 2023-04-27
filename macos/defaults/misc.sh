520e51df2dd485ef09b93a6172e0e4a665df2602
Nils <git@nkn-it.de>
2023-04-27 11:57:31 +0200

    AppleKeyboardUIMode

--- macos/defaults/misc.sh
+++ macos/defaults/misc.sh
@@ -31,3 +31,7 @@ defaults write com.apple.commerce AutoUpdate -bool true
 # Avoid creating .DS_Store files on network or USB volumes
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Enable full keyboard access for all controls
+# (e.g. enable Tab in modal dialogs)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
